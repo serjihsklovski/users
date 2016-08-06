9638414af503ab5a933bd5b3f55d7af6b04d06af
Serjih Sklovski <serjihsklovski@gmail.com>
2016-08-06 18:17:33 +0300

    method `UsersModel::get_users_list` was added

--- models/users_model.php
+++ models/users_model.php
@@ -8,8 +8,42 @@ class UsersModel {
       $conn = Database::connect();
       $res = $conn->query("SELECT FROM users WHERE id=$id");
       $res->setFetchMode(PDO::FETCH_ASSOC);
-      
+
       return $res->fetch();
     }
+  }
+
+  /*
+   * returns list of users
+   * list can be sorted ($sort = true)
+   */
+  public static function get_users_list($begin, $count, $sort=false, $desc=false, $field='id') {
+    $conn = Database::connect();
+    $users_list = array();
+    $sql = "SELECT id, uname, email, aedt FROM users";
+
+    if ($sort) {
+      $sql .= " ORDER BY $id";
+
+      if ($decl) {
+        $sql .= " DECL";
+      }
+    }
+
+    $sql .= " LIMIT $begin, $count";
+
+    $res = $conn->query($sql);
+
+    $i = 0;
+    while ($row = $res->fetch()) {
+      $users_list[$i]['id'] = $row['id'];
+      $users_list[$i]['uname'] = $row['uname'];
+      $users_list[$i]['email'] = $row['email'];
+      $users_list[$i]['aedt'] = $row['aedt'];
+
+      $i++;
+    }
+
+    return $users_list;
   }
 }
