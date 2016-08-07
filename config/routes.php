d8eed5cbd0da01203f50e8345da6d8cec6191793
Serjih Sklovski <serjihsklovski@gmail.com>
2016-08-07 03:36:18 +0300

    routes were added

--- config/routes.php
+++ config/routes.php
@@ -7,4 +7,7 @@ return array(
   'users/view/(\d+)/(\d+)/([01])' => 'users/view/$1/$2/$3',
   'users/view/(\d+)/(\d+)/([01])/([01])' => 'users/view/$1/$2/$3/$4',
   'users/view/(\d+)/(\d+)/([01])/([01])/([a-z_]+)' => 'users/view/$1/$2/$3/$4/$5',
+  'users/add/([\pL\s]+)/([a-z][a-z\d\._]*@[a-z][a-z\d\._]*\.[a-z][a-z\d\._])' => 'users/add/$1/$2',
+  'users/del/([\d]+)' => 'users/delete/$1',
+  'users/edt/([\d]+)/([\pL\s]+)/([a-z][a-z\d\._]*@[a-z][a-z\d\._]*\.[a-z][a-z\d\._])' => 'users/edit/$1/$2/$3',
 );
