6aa2cabd57fac6695eaefc50f77b65ffe8d74b24
Serjih Sklovski <serjihsklovski@gmail.com>
2016-08-07 17:19:19 +0300

    routes were fixed

--- config/routes.php
+++ config/routes.php
@@ -3,11 +3,10 @@
 return array(
   'users/view' => 'users/view',
   'users/view/(\d+)' => 'users/view/$1',
-  'users/view/(\d+)/(\d+)' => 'users/view/$1/$2',
-  'users/view/(\d+)/(\d+)/([01])' => 'users/view/$1/$2/$3',
-  'users/view/(\d+)/(\d+)/([01])/([01])' => 'users/view/$1/$2/$3/$4',
-  'users/view/(\d+)/(\d+)/([01])/([01])/([a-z_]+)' => 'users/view/$1/$2/$3/$4/$5',
-  'users/add/([\pL\s]+)/([a-z][a-z\d\._]*@[a-z][a-z\d\._]*\.[a-z][a-z\d\._])' => 'users/add/$1/$2',
-  'users/del/([\d]+)' => 'users/delete/$1',
-  'users/edt/([\d]+)/([\pL\s]+)/([a-z][a-z\d\._]*@[a-z][a-z\d\._]*\.[a-z][a-z\d\._])' => 'users/edit/$1/$2/$3',
+  'users/view/(\d+)/([01])' => 'users/view/$1/$2',
+  'users/view/(\d+)/([01])/([01])' => 'users/view/$1/$2/$3',
+  'users/view/(\d+)/([01])/([01])/([a-z_]+)' => 'users/view/$1/$2/$3/$4',
+  'users/add/([\pL\s]+)/([a-z][a-z\d\._]*@[a-z][a-z\d\._]*\.[a-z][a-z\d\._]*)' => 'users/add/$1/$2',
+  'users/del/(\d+)' => 'users/delete/$1',
+  'users/edt/(\d+)/([\pL\s]+)/([a-z][a-z\d\._]*@[a-z][a-z\d\._]*\.[a-z][a-z\d\._])' => 'users/edit/$1/$2/$3',
 );
