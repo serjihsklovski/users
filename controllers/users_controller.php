d140f8abe9f4057c38792dd14a3ba6fd473ec56a
Serjih Sklovski <serjihsklovski@gmail.com>
2016-08-07 17:31:55 +0300

    all actions delegate `UsersController::action_view`

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -3,30 +3,25 @@
 include_once ROOT . '/models/users_model.php';
 
 class UsersController {
-  public function action_view($lim_1=false, $lim_2=false, $sort=false, $desc=false, $field='id') {
-    $users_list = UsersModel::get_users_list($lim_1, $lim_2, $sort, $desc, $field);
+  public function action_view($page=0, $sort=false, $desc=false, $field='id') {
+    $users_list = UsersModel::get_users_list($page * 8, 8, $sort, $desc, $field);
+    $page_count = intval((UsersModel::row_count() - 1) / 8) + 1;
     require_once(ROOT . '/views/users/index.php');
     return true;
   }
 
   public function action_add($uname, $email) {
     UsersModel::insert_user($uname, $email, date("Y-m-d H:i:m"));
-    $users_list = UsersModel::get_users_list();
-    require_once(ROOT . '/views/users/index.php');
-    return true;
+    return $this->action_view(0);
   }
 
   public function action_delete($id) {
     UsersModel::delete_user($id);
-    $users_list = UsersModel::get_users_list();
-    require_once(ROOT . '/views/users/index.php');
-    return true;
+    return $this->action_view(0);
   }
 
   public function action_edit($id, $uname, $email) {
     UsersModel::update_user($id, $uname, $email);
-    $users_list = UsersModel::get_users_list();
-    require_once(ROOT . '/views/users/index.php');
-    return true;
+    return $this->action_view(0);
   }
 }
